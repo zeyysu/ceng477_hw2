ea955c3dd630ced20dbaaad697b04d50040da13c
zeyysu <zeynepsuylcn@gmail.com>
2022-12-15 04:11:32 +0300

    implement transformations

---
+++ Methods.cpp
@@ -0,0 +1,275 @@
+#include "Methods.h"
+
+
+Matrix4 createTranslationMatrix(Translation *t){
+	Matrix4 matrix = getIdentityMatrix();
+	matrix.val[0][3] = t->tx;
+	matrix.val[1][3] = t->ty;
+	matrix.val[2][3] = t->tz;
+	return matrix;
+}
+
+Matrix4 createScalingMatrix(Scaling *s){
+	Matrix4 matrix;
+	matrix.val[0][0] = s->sx;
+	matrix.val[0][1] = 0;
+	matrix.val[0][2] = 0;
+	matrix.val[0][3] = 0;
+
+	matrix.val[1][1] = s->sy;
+	matrix.val[1][0] = 0;
+	matrix.val[1][2] = 0;
+	matrix.val[1][3] = 0;
+
+	matrix.val[2][2] = s->sz;
+	matrix.val[2][1] = 0;
+	matrix.val[2][0] = 0;
+	matrix.val[2][3] = 0;
+
+	matrix.val[3][0] = 0;
+	matrix.val[3][1] = 0;
+	matrix.val[3][2] = 0;
+	matrix.val[3][3] = 1;
+
+	return matrix;
+}
+
+Matrix4 createRotationMatrix(Rotation *r){
+	//alternative method
+	Vec3 u;
+	u.x = r->ux;
+	u.y = r->uy;
+	u.z = r->uz;
+
+	u = normalizeVec3(u);
+
+	Vec3 v;
+	if(u.x<=u.y){
+		if(u.x<=u.z){
+			v.x = 0;
+			v.y = -1*u.z;
+			v.z = v.y;
+		}
+		else{
+			v.z = 0;
+			v.y = -1*u.x;
+			v.x = v.y;
+		}
+	}
+	else{
+		if(u.y<=u.z){
+			v.y = 0;
+			v.x = -1*u.z;
+			v.z = v.x;
+		}
+		else{
+			v.z = 0;
+			v.y = -1*u.x;
+			v.x = v.y;
+		}
+	}
+
+	v = normalizeVec3(v);
+	Vec3 w = crossProductVec3(u,v);
+	w = normalizeVec3(w);
+	Matrix4 M;
+	M.val[0][0] = u.x;
+	M.val[0][1] = u.y;
+	M.val[0][2] = u.z;
+	M.val[0][3] = 0; 
+
+	M.val[1][0] = v.x;
+	M.val[1][1] = v.y;
+	M.val[1][2] = v.z;
+	M.val[1][3] = 0; 
+
+	M.val[2][0] = w.x;
+	M.val[2][1] = w.y;
+	M.val[2][2] = w.z;
+	M.val[2][3] = 0; 
+
+	M.val[3][0] = 0;
+	M.val[3][1] = 0;
+	M.val[3][2] = 0;
+	M.val[3][3] = 1;
+
+	Matrix4 R;
+	R.val[0][0] = 1;
+	R.val[0][1] = 0;
+	R.val[0][2] = 0;
+	R.val[0][3] = 0;
+
+	R.val[1][0] = 0;
+	R.val[1][1] = cos(r->angle);
+	R.val[1][2] = -1*sin(r->angle);
+	R.val[1][3] = 0;
+
+	R.val[2][0] = 0;
+	R.val[2][1] = sin(r->angle);
+	R.val[2][2] = cos(r->angle);
+	R.val[2][3] = 0;
+
+	R.val[3][0] = 0;
+	R.val[3][1] = 0;
+	R.val[3][2] = 0;
+	R.val[3][3] = 1;
+
+	Matrix4 M_1;
+	M_1.val[0][0] = u.x;
+	M_1.val[0][1] = v.x;
+	M_1.val[0][2] = w.x;
+	M_1.val[0][3] = 0; 
+
+	M_1.val[1][0] = u.y;
+	M_1.val[1][1] = v.y;
+	M_1.val[1][2] = w.y;
+	M_1.val[1][3] = 0; 
+
+	M_1.val[2][0] = u.z;
+	M_1.val[2][1] = v.z;
+	M_1.val[2][2] = w.z;
+	M_1.val[2][3] = 0; 
+
+	M_1.val[3][0] = 0;
+	M_1.val[3][1] = 0;
+	M_1.val[3][2] = 0;
+	M_1.val[3][3] = 1;
+
+	
+	Matrix4 matrix = multiplyMatrixWithMatrix(R,M);
+	matrix = multiplyMatrixWithMatrix(M_1, matrix);
+	return matrix;
+	
+}
+
+
+Matrix4 modelingTransformations(Scene *scene, Mesh *mesh){
+	int numberOfTransformations = mesh->numberOfTransformations;
+	int i;
+	Matrix4 matrix = getIdentityMatrix();
+	for(i=0;i<numberOfTransformations;i++){
+		switch(mesh->transformationTypes[i]){
+			case 't':
+				Matrix4 mt = createTranslationMatrix(scene->translations[mesh->transformationIds[i]]);
+				matrix = multiplyMatrixWithMatrix(mt, matrix);
+				break;
+			case 's':
+				Matrix4 ms = createScalingMatrix(scene->scalings[mesh->transformationIds[i]]);
+				matrix = multiplyMatrixWithMatrix(ms, matrix);
+				break;
+			case 'r':
+				Matrix4 mr = createRotationMatrix(scene->rotations[mesh->transformationIds[i]]);
+				matrix = multiplyMatrixWithMatrix(ms, matrix);
+				break;
+			default:
+				break;
+		}
+	}
+	return matrix;
+};
+
+Matrix4 cameraTransformation(Camera *camera){
+	Matrix4 matrix;
+	matrix.val[0][0] = camera->u.x;
+    matrix.val[0][1] = camera->u.y;
+    matrix.val[0][2] = camera->u.z;
+   	matrix.val[0][3] = -1*(camera->u.x*camera->pos.x + camera->u.y*camera->pos.y + camera->u.z*camera->pos.z);
+
+    matrix.val[1][0] = camera->v.x;
+    matrix.val[1][1] = camera->v.y;
+    matrix.val[1][2] = camera->v.z;
+    matrix.val[1][3] = -1*(camera->v.x*camera->pos.x + camera->v.y*camera->pos.y + camera->v.z*camera->pos.z);
+
+    matrix.val[2][0] = camera->w.x;
+    matrix.val[2][1] = camera->w.y;
+    matrix.val[2][2] = camera->w.z;
+    matrix.val[2][3] = -1*(camera->w.x*camera->pos.x + camera->w.y*camera->pos.y + camera->w.z*camera->pos.z);
+
+    matrix.val[3][0] = 0.0;
+    matrix.val[3][1] = 0.0;
+    matrix.val[3][2] = 0.0;
+    matrix.val[3][3] = 1.0;
+
+	return matrix;
+
+}
+
+Matrix4 createOrtMatrix(Camera *camera){
+	Matrix4 matrix;
+
+	matrix.val[0][0] = 2/(camera->right - camera->left);
+	matrix.val[0][1] = 0;
+	matrix.val[0][2] = 0;
+	matrix.val[0][3] = -1*(camera->right + camera->left)/(camera->right - camera->left);
+
+	matrix.val[1][0] = 0;
+	matrix.val[1][1] =  2/(camera->top - camera->bottom);
+	matrix.val[1][2] = 0;
+	matrix.val[1][3] = -1*(camera->top + camera->bottom)/(camera->top - camera->bottom);
+
+	matrix.val[2][0] = 0;
+	matrix.val[2][1] = 0;
+	matrix.val[2][2] =  -1*2/(camera->far - camera->near);
+	matrix.val[2][3] = -1*(camera->far + camera->near)/(camera->far - camera->near);
+
+	matrix.val[3][0] = 0;
+	matrix.val[3][1] = 0;
+	matrix.val[3][2] = 0;
+	matrix.val[3][3] = 1;
+
+	return matrix;
+
+}
+
+Matrix4 createPersMatrix(Camera *camera){
+	Matrix4 matrix;
+
+	matrix.val[0][0] = (2*camera->near)/(camera->right - camera->left);
+	matrix.val[0][1] = 0;
+	matrix.val[0][2] = (camera->right + camera->left)/(camera->right - camera->left);
+	matrix.val[0][3] = 0;
+
+	matrix.val[1][0] = 0;
+	matrix.val[1][1] =  (2*camera->near)/(camera->top - camera->bottom);
+	matrix.val[1][2] = (camera->top + camera->bottom)/(camera->top - camera->bottom);
+	matrix.val[1][3] = 0;
+
+	matrix.val[2][0] = 0;
+	matrix.val[2][1] = 0;
+	matrix.val[2][2] =  -1*(camera->far + camera->near)/(camera->far - camera->near);
+	matrix.val[2][3] = -2*(camera->far * camera->near)/(camera->far - camera->near);
+
+	matrix.val[3][0] = 0;
+	matrix.val[3][1] = 0;
+	matrix.val[3][2] = -1;
+	matrix.val[3][3] = 0;
+
+	return matrix;
+}
+
+Matrix4 createViewportMatrix(Camera *camera){
+	Matrix4 matrix;
+
+	matrix.val[0][0] = camera->horRes/2;
+	matrix.val[0][1] = 0;
+	matrix.val[0][2] = 0;
+	matrix.val[0][3] = (camera->horRes-1)/2;
+
+	matrix.val[1][0] = 0;
+	matrix.val[1][1] = camera->verRes/2;
+	matrix.val[1][2] = 0;
+	matrix.val[1][3] = (camera->verRes-1)/2;
+
+	matrix.val[2][0] = 0;
+	matrix.val[2][1] = 0;
+	matrix.val[2][2] = 1/2;
+	matrix.val[2][3] = 1/2;
+
+	matrix.val[3][0] = 0;
+	matrix.val[3][1] = 0;
+	matrix.val[3][2] = 0;
+	matrix.val[3][3] = 0;
+
+	return matrix;
+}
+
---
+++ Methods.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <cmath>
+
+#include "Scene.h"
+#include "Camera.h"
+#include "Color.h"
+#include "Mesh.h"
+#include "Rotation.h"
+#include "Scaling.h"
+#include "Translation.h"
+#include "Triangle.h"
+#include "Vec3.h"
+#include "tinyxml2.h"
+#include "Helpers.h"
+#include "Matrix4.h"
+
+Matrix4 cameraTransformation(Camera *);
+Matrix4 createOrtMatrix(Camera *);
+Matrix4 createPersMatrix(Camera *);
+Matrix4 createViewportMatrix(Camera *);
+Matrix4 modelingTransformations(Scene *, Mesh *);
